47a73b1f17f376ba848373d520679c5b89505248
bixynn2027 <59707557+bixynn2027@users.noreply.github.com>
2020-01-27 01:14:20 +0900

    実行結果の調整
    例の通りに実行結果が表示されるように処理の調整

--- prob3-16/prob3-16/Program.cs
+++ prob3-16/prob3-16/Program.cs
@@ -12,17 +12,17 @@ namespace prob3_16
         {
             Console.Write("整数値を入力：");
             int num = int.Parse(Console.ReadLine());
-            if(num % 2 == 0)
-            {
-                Console.WriteLine("2の倍数です");
-            }
-            if (num % 3 == 0)
-            {
-                Console.WriteLine("3の倍数です");
-            }
             if (num % 2 == 0 && num % 3 == 0)
             {
                 Console.WriteLine("2と3の公倍数です");
+            }           
+            else if (num % 3 == 0)
+            {
+                    Console.WriteLine("3の倍数です");
+            }
+            else if (num % 2 == 0)
+            {
+                    Console.WriteLine("2の倍数です");
             }
         }
     }
